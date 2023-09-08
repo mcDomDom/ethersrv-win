bd57bca7e39fe1fa1b754ae8c83b2303917f984f
mcDomDom <kazuya@sankisetubi.co.jp>
2023-09-08 17:25:28 +0900

    Delete unnecessary code

--- ethersrv-win.c
+++ ethersrv-win.c
@@ -819,8 +819,6 @@ int main(int argc, char **argv) {
   pcap_if_t	*devs[100];
   HANDLE hMutex;
 
-  DWORD dwRet = GetShortPathName("D:\\ap2_inner1.jpg", szAdapterName, 260);
-
   /* I expect at least two positional arguments, and not more than 26 */
   if (argc  < 2 || argc  > 26) {
     help();
